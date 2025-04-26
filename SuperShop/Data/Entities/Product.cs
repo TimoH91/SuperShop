201ac11db3101cbc96ab64164d702134247a60e1
TimoH91 <timothyharris04@gmail.com>
2025-04-26 15:12:10 +0100

    Modify products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -6,8 +6,10 @@ namespace SuperShop.Data.Entities
     public class Product
     {
 
-        public int Id { get; set; } 
+        public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} cannot contain more than {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -18,11 +20,11 @@ namespace SuperShop.Data.Entities
 
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
     
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
