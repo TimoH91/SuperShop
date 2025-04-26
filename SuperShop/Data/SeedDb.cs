cb2dec85d13419c6a18f1c95e34f7351675b639d
TimoH91 <timothyharris04@gmail.com>
2025-04-26 16:21:23 +0100

    Seed of database created

---
+++ SuperShop/Data/SeedDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using SQLitePCL;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private Random _random;
+        
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if (!_context.Products.Any())
+            {
+                AddProduct("IPhone X");
+                AddProduct("Apple Headphones");
+                AddProduct("AOC 24 Monitor");
+                AddProduct("Asus Zenbook 14");
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            _context.Products.Add(new Product
+            {
+                Name = name,
+                Price = _random.Next(1000),
+                IsAvailable = true,
+                stock = _random.Next(100)
+            });
+
+        }
+    }
+}
