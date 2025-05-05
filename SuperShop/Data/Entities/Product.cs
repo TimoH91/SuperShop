a0369a64f8c9c6cd97ef846f5d85bb0f659e9568
TimoH91 <timothyharris04@gmail.com>
2025-05-05 18:17:06 +0100

    Add and edit images Images on API Products User on API Products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -33,5 +33,19 @@ namespace SuperShop.Data.Entities
         public double stock { get; set; }
 
         public User User { get; set; }
+
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://localhost:44380{ImageUrl.Substring(1)}";
+            }
+          
+        }
     }
 }
